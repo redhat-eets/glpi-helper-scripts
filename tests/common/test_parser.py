1d22fdb9933d009e4c87a25e0aa63d3f02f97fdb
Eric Zhang <erzhang@erzhang.remote.csb>
2023-07-10 13:19:11 -0400

    changes to test_parser, using a method to test

--- tests/common/test_parser.py
+++ tests/common/test_parser.py
@@ -1,24 +1,12 @@
 import sys
-import os
-import subprocess
 
-sys.path.append("..")
+sys.path.append("../..")
+
+from common.parser import argparser
+
 
 
 def test_create_parser():
-    ip = "10.19.111.54"
-    user_token = "qBIErMvrucSzVhwY92rC6bMq7Tj22N0nlCmhAxaO"
-    os.environ["GLPI_INSTANCE"] = ip
-    os.environ["GLPI_TOKEN"] = user_token
-    output = subprocess.check_output(["./parser.py"], cwd="../../common")
-    out = output.split()
-    assert ip == out[0].decode("ascii")
-    assert user_token == out[1].decode("ascii")
-    os.environ["GLPI_INSTANCE"] = "1"
-    os.environ["GLPI_TOKEN"] = "2"
-    output = subprocess.check_output(
-        ["./parser.py", "-i", ip, "-t", user_token], cwd="../../common"
-    )
-    out = output.split()
-    assert ip == out[0].decode("ascii")
-    assert user_token == out[1].decode("ascii")
+    parser = argparser()
+    parser.test_parser
+
